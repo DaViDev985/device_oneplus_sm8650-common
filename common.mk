5eb185f4e8ef32dfdbe4b081882fd607a0b3e8a5
DaViDev985 <davidev985@gmail.com>
2025-02-02 12:27:00 +0100

    sm8650-common: Add blueprint-based Google Apps debloat
    This works by adding a dummy .apk file (literally the default Android Studio project) that overrides all the packages you wish to remove.
    
    Future changes will include building the .apk during the build process (instead of including a presigned and prebuilt file) in order to publish the source files for the dummy app.

--- common.mk
+++ common.mk
@@ -337,6 +337,10 @@ PRODUCT_PACKAGES += \
     qspa_vendor.rc \
     vendor.qti.qspa-service
 
+# Remove Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # SecureElement
 ifneq ($(TARGET_IS_TABLET),true)
 PRODUCT_PACKAGES += \
